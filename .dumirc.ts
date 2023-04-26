ebf726180ecb108ff2ca766b1e5bc1983b2a1bc9
GYBSL <1290005228@qq.com>
2023-04-26 21:31:22 +0800

    同步代码

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: '编程文档',
-    logo: "/logo.jpg",
+    logo: "./logo.jpg",
     footer: "别看我,看代码！"
   },
   history: {
     type: "hash"
   },
   base: "/", // 文档起始路由
-  publicPath: "/docs-dist/" // 静态资源起始路径
+  publicPath: "/code_docs/" // 静态资源起始路径
 });
