201eccd0bf663ce04c00564d730f617b010eb8a7
GYBSL <1290005228@qq.com>
2023-05-05 14:37:30 +0800

    添加注释

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: 'docs-dist', // 打包之后的文件夹
   themeConfig: {
     name: '编程文档',
     logo: "/code_docs/logo.jpg",
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   favicons: ['/code_docs/favicon.ico'],
   history: {
-    type: "hash"
+    type: "hash" // hash模式
   },
   base: "/", // 文档起始路由
   publicPath: "/code_docs/" // 静态资源起始路径
