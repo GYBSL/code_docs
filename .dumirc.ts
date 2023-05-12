662e0b531f851a466c3da721cf0cc40c9465c045
GYBSL <1290005228@qq.com>
2023-05-12 09:52:51 +0800

    改footer

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   themeConfig: {
     name: '编程文档',
     logo: "/code_docs/logo.jpg",
-    footer: "别看我,看代码！👀"
+    footer: "“无人问津也好，技不如人也罢，你都要试着安静下来，去做自己该做的事，而不是让内心的烦躁焦虑毁掉你本就不多的热情和定力” ——人民日报"
   },
   favicons: ['/code_docs/favicon.ico'],
   history: {
