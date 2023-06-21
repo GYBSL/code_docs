f84465d279d43f8a31358d571f93fcba5e9aa3fc
GYBSL <1290005228@qq.com>
2023-06-21 17:50:40 +0800

    更换logo和ico

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   outputPath: 'docs-dist', // 打包之后的文件夹
   themeConfig: {
     name: '编程文档',
-    logo: "/code_docs/logo.jpg",
+    logo: "/code_docs/logo.webp",
     footer: "“无人问津也好，技不如人也罢，你都要试着安静下来，去做自己该做的事，而不是让内心的烦躁焦虑毁掉你本就不多的热情和定力” ——人民日报"
   },
-  favicons: ['/code_docs/favicon.ico'],
+  favicons: ['/code_docs/logo.ico'],
   history: {
     type: "hash" // hash模式
   },
